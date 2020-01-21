1267a2f701e8f3ae7c3dda6e53440aa4e9b746c5
ClAndrew <cmlandrews@gmail.com>
2020-01-20 17:24:25 -0800

    Save

--- src/win32/Graphics.h
+++ src/win32/Graphics.h
@@ -1,5 +1,150 @@
 #pragma once
 
+struct Coordinate
+{
+	void Initialize(int x, int y);
+
+	int X;
+	int Y;
+};
+
+struct FourCoordinates
+{
+	void Initialize(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3);
+
+	Coordinate Location[4];
+};
+
+struct PieceLayout
+{
+	void Initialize(int numberOfRotationFrames);
+
+	void AddRotation(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3);
+
+	std::vector<FourCoordinates> EachRotation;
+};
+
+enum Color
+{
+	Red,
+	Blue,
+	Green,
+	Cyan,
+	Magenta,
+	Yellow,
+	Purple
+};
+
+class Random
+{
+public:
+	Random();
+
+	int Next(int n);
+};
+
+enum class DropPieceResult { None, PieceLanded, RowsCleared, GameOver };
+
+class Grid
+{
+	std::vector<int> data;
+
+	std::vector<PieceLayout> pieceLayouts;
+	std::vector<Color> pieceColors;
+	std::vector<int> statistics;
+
+	int currentPieceType;
+	int nextPieceType;
+	int currentPieceRotation;
+	Coordinate currentPieceLocation;
+	int lineClearCount;
+	int score;
+	int maxForceDropThisPiece;
+	int currentForceDropLengthThisPiece;
+
+	std::vector<int> rowsBeingCleared;
+
+	Random random;
+
+public:
+	Grid();
+
+	void Reset();
+
+	bool SetCell(Coordinate location, int value, bool allowOverwrite);
+
+	bool SetCell(int x, int y, int value, bool allowOverwrite);
+
+	int GetCell(int x, int y);
+
+	void ValidateBounds(int x, int y);
+
+	bool TryRotatePiece();
+
+	// Returns whether we could do it.
+	bool TryMovePiece(int x, int y);
+
+	bool MovePieceLeft();
+
+	bool MovePieceRight();
+
+	DropPieceResult DropPiece(bool forcedDrop);
+
+	bool IsRowClear(int y);
+
+	void MoveEverythingDown(int yStoppingPoint);
+
+	void ProcessAnyClearedRows();
+
+	std::vector<int> GetRowsBeingCleared();
+
+	void PurgeClearedRows();
+
+	void NewPiece();
+
+	bool CommitCurrentPiece();
+
+	bool CanPlacePieceAt(FourCoordinates coordinates);
+
+	FourCoordinates GetPieceCoordinates(Coordinate pieceLocation, int pieceType, int pieceRotation);
+
+	FourCoordinates GetPieceCoordinates(int pieceType);
+
+	FourCoordinates GetCurrentPieceCoordinates();
+
+	FourCoordinates GetNextPieceCoordinates();
+
+	int GetLinesCleared();
+
+	int GetScore();
+
+	int GetNextPieceType();
+
+	Coordinate GetCurrentPieceLocation();
+
+	int GetCurrentPieceRotation();
+
+	Color GetColor(int index);
+
+	int GetCurrentPieceType();
+
+	int GetStatistic(int pieceType);
+};
+
+class RowClearingAnimation
+{
+	int frames = -1;
+
+public:
+	void Start();
+
+	bool IsAnimating();
+
+	void Stop();
+
+	void Update();
+};
+
 class Graphics
 {
 	ComPtr<IWICImagingFactory> m_wicImagingFactory;
@@ -13,6 +158,31 @@ class Graphics
 	ComPtr<ID2D1Bitmap1> m_blocks;
 
 	enum class PieceType { I, L, O, R, S, T, Z };
+
+	// Gameplay
+	Grid grid;
+	int framesPerPieceDrop;
+	int framesUntilPieceDrop;
+	bool forcingDrop;
+	bool gameOver;
+	bool loserMode;
+	RowClearingAnimation rowClearingAnimation;
+	Random random;
+
+	// Camera settings
+	float cameraX;
+	float cameraY;
+	float cameraTargetX;
+	float cameraTargetY;
+	float cameraRotation;
+	float cameraTargetRotation;
+	bool showDebuggingAids;
+
+	// Graphics resources
+	bool prebakedDrawingValid;
+	bool finishedLoadingResources;
+	float backgroundScrollX;
+	float backgroundScrollY;
 
 public:
 	void Initialize(HWND hwnd);
